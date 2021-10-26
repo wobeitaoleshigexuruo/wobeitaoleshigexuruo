977a6352f9c63a34ce08062965ced2fc26b54ce2
brian <90851827+wobeitaoleshigexuruo@users.noreply.github.com>
2021-10-26 21:24:44 +0800

    Add files via upload

---
+++ 华氏温度/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int f;
+    f=100;
+    double c = 5.0*(f-32)/9;
+    printf("%lf",c);
+    return 0;
+}
---
+++ 华氏温度改错/华氏温度改错/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   /*
+    int celsius,fahr;
+
+    fahr=100;
+
+    celsius=5*(fahr-32)/9;
+
+    printf("fahr=%d,celsius=%d\n",fahr,celsius);
+    */\
+
+    int i=11,l;
+    for(i = 11; i <= 20; i++)
+        {
+    for(l = 1; l <= 10; l++){
+        printf( "%d", l );
+    }
+    printf("\n");
+        }
+
+
+
+
+    return 0;
+}
